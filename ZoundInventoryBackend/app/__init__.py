e1edc270094efc862b08b4ab839d36e02c0778cb
David Vergara <davidvergara0606@gmail.com>
2026-07-01 09:53:49 -0500

    Cambios

--- ZoundInventoryBackend/app/__init__.py
+++ ZoundInventoryBackend/app/__init__.py
@@ -5,8 +5,7 @@ from app.config.settings import Config
 from app.database.database import db
 
 
-import app.models 
-
+from app.models.usuario import Usuario 
 
 migrate = Migrate()
 
@@ -16,17 +15,13 @@ def create_app():
     app.config.from_object(Config)
     
 
-    CORS (app)
+    CORS(app, resources={r"/*": {"origins": "*"}})
     
     db.init_app(app)
-    
-    
     migrate.init_app(app, db)
     
 
     from app.routes.auth import auth_bp
     app.register_blueprint(auth_bp, url_prefix='/auth')
-
-
 
     return app
--- ZoundInventoryBackend/app/routes/auth.py
+++ ZoundInventoryBackend/app/routes/auth.py
@@ -1,36 +1,44 @@
 from flask import Blueprint, request, jsonify
 from app.database.database import db
 from app.models.usuario import Usuario
-
+from werkzeug.security import generate_password_hash, check_password_hash
 
 auth_bp = Blueprint('auth', __name__)
 
-
-@auth_bp.route('/registro', methods=['POST'])
-def registro():
+@auth_bp.route('/admin/registro', methods=['POST'])
+def registro_admin():
     data = request.json
+    
+    admin_id = data.get('admin_id') 
+    admin = Usuario.query.get(admin_id)
+    
+    if not admin or admin.id_rol != 1:
+        return jsonify({"mensaje": "Acceso denegado: Solo administradores pueden registrar usuarios"}), 403
+
+
+    password_encriptada = generate_password_hash(data['password'])
+    
     nuevo_usuario = Usuario(
         primer_nombre=data['primer_nombre'],
         primer_apellido=data['primer_apellido'],
         tipo_documento=data['tipo_documento'],
         documento=data['documento'],
         correo=data['correo'], 
-        password=data['password'],
+        password=password_encriptada,
         id_rol=data['id_rol']
     )
+    
     db.session.add(nuevo_usuario)
     db.session.commit()
-    
-    return jsonify({"mensaje": "Usuario creado exitosamente"}), 201
-
+    return jsonify({"mensaje": "Usuario registrado exitosamente"}), 201
 
 @auth_bp.route('/login', methods=['POST'])
 def login():
-
     data = request.json
-    usuario = Usuario.query.filter_by(documento=data['documento'], password=data['password']).first()
+    usuario = Usuario.query.filter_by(documento=data['documento']).first()
     
-    if usuario:
+
+    if usuario and check_password_hash(usuario.password, data['password']):
         return jsonify({"mensaje": "Login exitoso", "usuario": usuario.primer_nombre}), 200
     else:
         return jsonify({"mensaje": "Credenciales inválidas"}), 401
